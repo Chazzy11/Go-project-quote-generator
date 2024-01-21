00acb54e5125c493defbc876a530f6611c397613
Chazzy11 <charlotterogers96@hotmail.com>
2024-01-21 14:58:12 +0000

    variables and struct created

--- main.go
+++ main.go
@@ -10,3 +10,12 @@ import (
 )
 
 // create variables for upper lower bounds of credit score
+
+const creditScoreMin = 500
+const creditScoreMax = 900
+
+// create struct for credit score data
+
+type credit_score struct {
+	Score int `json:"credit_score"`
+}
