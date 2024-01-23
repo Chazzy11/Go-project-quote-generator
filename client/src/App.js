962eed676914547469f7bb7171994829a065693d
Chazzy11 <charlotterogers96@hotmail.com>
2024-01-23 11:31:06 +0000

    created button quote components

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <main className="Quote-container">
         <div>
           <h1>Hello</h1>
         </div>
-      </header>
+      </main>
     </div>
   );
 }
