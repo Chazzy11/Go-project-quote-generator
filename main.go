a965d75bc3e4dab864b07c5184055decc063e717
Chazzy11 <charlotterogers96@hotmail.com>
2024-01-23 10:43:38 +0000

    api altered to generate quote numbers

--- main.go
+++ main.go
@@ -11,33 +11,33 @@ import (
 
 // create variables for upper lower bounds of credit score
 
-const creditScoreMin = 500
-const creditScoreMax = 900
+const quoteNumMin = 1
+const quoteNumMax = 9
 
 // create struct for credit score data
 
-type credit_score struct {
-	CreditScore int `json:"credit_score"`
+type quotenumber struct {
+	QuoteNumber int `json:"quotenumber"`
 }
 
 // create function to generate random credit score
 // this function is also http handler function - use http.ResponseWriter and http.Request
 // inside function, credit score is generated, passed to struct, and written to HTTP response as json
 
-func generateCreditScore(w http.ResponseWriter, r *http.Request) {
-	var creditScore = credit_score{
-		CreditScore: (rand.Intn(creditScoreMax-creditScoreMin) + creditScoreMin),
+func generateQuoteNumber(w http.ResponseWriter, r *http.Request) {
+	var quote = quotenumber{
+		QuoteNumber: (rand.Intn(quoteNumMax-quoteNumMin) + quoteNumMin),
 	} 
 
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(creditScore)
+	json.NewEncoder(w).Encode(quote)
 
 	}
 
 // create main function to create emdpoint and listen on port 8080
 
 func handleRequests() {
-	http.Handle("/credit_score", http.HandlerFunc(generateCreditScore))
+	http.Handle("/quotenumber", http.HandlerFunc(generateQuoteNumber))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
